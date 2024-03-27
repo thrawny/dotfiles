432398c28634ebf31179f00094afcc32aa1ee1ef
Jonas Lergell <jonas.lergell@volvocars.com>
2024-03-27 13:13:37 +0100

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,31 @@
-require('setup-lazy')
+require("setup-lazy")
 
-vim.g.mapleader = ','
+vim.g.mapleader = ","
 
-require('plugins')
+require("plugins")
 
 local set = vim.opt
 local cmd = vim.cmd
 local o = vim.o
 local map = vim.api.nvim_set_keymap
 
-o.encoding = 'utf-8'
-o.backspace = 'indent,eol,start'
+o.encoding = "utf-8"
+o.backspace = "indent,eol,start"
 o.ruler = true
 o.expandtab = true
 o.shiftwidth = 2
 o.softtabstop = 2
+o.tabstop = 4
 o.number = true
 o.numberwidth = 3
 o.hidden = true
 o.tw = 80
-o.mouse = 'a'
+o.mouse = "a"
 o.timeoutlen = 1000
 o.ttimeoutlen = 10
-o.backupdir = vim.fn.expand('~/.config/nvim/backup//')
-o.directory = vim.fn.expand('~/.config/nvim/swap//')
-o.undodir = vim.fn.expand('~/.config/nvim/undo//')
+o.backupdir = vim.fn.expand("~/.config/nvim/backup//")
+o.directory = vim.fn.expand("~/.config/nvim/swap//")
+o.undodir = vim.fn.expand("~/.config/nvim/undo//")
 o.backup = true
 o.swapfile = false
 o.autoread = true
@@ -39,45 +40,45 @@ o.ignorecase = true
 o.smartcase = true
 
 -- Insert mode mappings
-map('i', '{<CR>', '{<CR>}<Esc>ko', {noremap = true})
-map('i', '({<CR>', '({<CR>});<Esc>ko', {noremap = true})
+map("i", "{<CR>", "{<CR>}<Esc>ko", { noremap = true })
+map("i", "({<CR>", "({<CR>});<Esc>ko", { noremap = true })
 
 -- Normal mode mappings for easier moving between buffers
-map('n', '<Leader>n', '<esc>:bp<CR>', {noremap = true})
-map('n', '<Leader>m', '<esc>:bn<CR>', {noremap = true})
+map("n", "<Leader>n", "<esc>:bp<CR>", { noremap = true })
+map("n", "<Leader>m", "<esc>:bn<CR>", { noremap = true })
 
 -- Visual mode mapping for sorting
-map('v', '<Leader>s', ':sort<CR>', {noremap = true})
+map("v", "<Leader>s", ":sort<CR>", { noremap = true })
 
 -- Normal mode mappings
-map('n', '<Leader>lc', ':lclose<CR>', {noremap = true})
-map('n', '<Leader>o', ':noh<ESC>', {noremap = true, silent = true})
-map('n', 'Y', 'y$', {noremap = true})
-map('n', '<C-d>', '<C-d>zz', {noremap = true})
-map('n', '<C-u>', '<C-u>zz', {noremap = true})
-map('n', 'n', 'nzzzv', {noremap = true})
-map('n', 'N', 'Nzzzv', {noremap = true})
-map('n', '<Leader>,', '<C-^>zz', {noremap = true})
-map('n', '<Leader>v', ':e $MYVIMRC<CR>', {noremap = true})
-map('n', '<Leader>z', ':e ~/.zshrc<CR>', {noremap = true})
-map('n', '<Leader>i', ':e ~/dotfiles/linux/i3/config<CR>', {noremap = true})
-map('n', '<Leader>tm', ':e ~/.tmux.conf<CR>', {noremap = true})
-map('n', '<Leader>sv', ':source $MYVIMRC<CR>', {noremap = true})
-map('n', '<localleader>b', ':NERDTreeToggle<CR>', {noremap = true})
-map('n', '<localleader>,', ',', {noremap = true})
+map("n", "<Leader>lc", ":lclose<CR>", { noremap = true })
+map("n", "<Leader>o", ":noh<ESC>", { noremap = true, silent = true })
+map("n", "Y", "y$", { noremap = true })
+map("n", "<C-d>", "<C-d>zz", { noremap = true })
+map("n", "<C-u>", "<C-u>zz", { noremap = true })
+map("n", "n", "nzzzv", { noremap = true })
+map("n", "N", "Nzzzv", { noremap = true })
+map("n", "<Leader>,", "<C-^>zz", { noremap = true })
+map("n", "<Leader>v", ":e $MYVIMRC<CR>", { noremap = true })
+map("n", "<Leader>z", ":e ~/.zshrc<CR>", { noremap = true })
+map("n", "<Leader>i", ":e ~/dotfiles/linux/i3/config<CR>", { noremap = true })
+map("n", "<Leader>tm", ":e ~/.tmux.conf<CR>", { noremap = true })
+map("n", "<Leader>sv", ":source $MYVIMRC<CR>", { noremap = true })
+map("n", "<localleader>b", ":NERDTreeToggle<CR>", { noremap = true })
+map("n", "<localleader>,", ",", { noremap = true })
 
 -- Expression mapping (might need adjustment for Lua)
 -- map('n', 'gp', '`[' .. vim.fn.strpart(vim.fn.getregtype(), 0, 1) .. '`]', {noremap = true, expr = true})
 
 -- Visual mode remap for searching the selected text
-map('v', '//', 'y/\\V<C-R>=escape(@",\'/\\\')<CR><CR>', {noremap = true})
+map("v", "//", "y/\\V<C-R>=escape(@\",'/\\')<CR><CR>", { noremap = true })
 
 -- Clipboard copy paste
-map('', '<space>y', '"*y', {noremap = true})
-map('', '<space>Y', '"*Y', {noremap = true})
-map('', '<space>p', '"*p', {noremap = true})
-map('', '<space>P', '"*P', {noremap = true})
+map("", "<space>y", '"*y', { noremap = true })
+map("", "<space>Y", '"*Y', { noremap = true })
+map("", "<space>p", '"*p', { noremap = true })
+map("", "<space>P", '"*P', { noremap = true })
 
 -- Looks
 vim.g.molokai_original = 1
-cmd('colorscheme molokai')
+cmd("colorscheme molokai")
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,65 @@
+require("lazy").setup({
+	"tpope/vim-surround",
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+		{
+			"williamboman/mason.nvim",
+		},
+		{
+			"nvimtools/none-ls.nvim",
+		},
+		{
+			"stevearc/conform.nvim",
+			event = { "BufWritePre" },
+			cmd = { "ConformInfo" },
+			keys = {
+				{
+					-- Customize or remove this keymap to your liking
+					"<leader>f",
+					function()
+						require("conform").format({ async = true, lsp_fallback = true })
+					end,
+					mode = "",
+					desc = "Format buffer",
+				},
+			},
+			-- Everything in opts will be passed to setup()
+			opts = {
+				-- Define your formatters
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					javascript = { { "prettierd", "prettier" } },
+				},
+				-- Set up format-on-save
+				format_on_save = { timeout_ms = 500, lsp_fallback = true },
+				-- Customize formatters
+				formatters = {
+					shfmt = {
+						prepend_args = { "-i", "2" },
+					},
+				},
+			},
+			init = function()
+				-- If you want the formatexpr, here is the place to set it
+				vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+			end,
+		},
+	},
+})
+
+require("mason").setup()
