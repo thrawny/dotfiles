d9d3e6cc3a7b64ce14b61e0e0dd2e978b6d1819b
Jonas Lergell <jonaslergell@gmail.com>
2025-09-19 09:59:56 +0200

    npm install -g

--- nix/modules/home-manager/npm.nix
+++ nix/modules/home-manager/npm.nix
@@ -2,4 +2,18 @@
 {
   home.file.".default-npm-packages".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/config/npm/default-packages";
+
+  # Configure npm to use a writable directory for global packages
+  home.file.".npmrc".text = ''
+    prefix = ${config.home.homeDirectory}/.npm-global
+  '';
+
+  # Add npm global bin to PATH
+  home.sessionVariables = {
+    NPM_CONFIG_PREFIX = "${config.home.homeDirectory}/.npm-global";
+  };
+
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.npm-global/bin"
+  ];
 }
