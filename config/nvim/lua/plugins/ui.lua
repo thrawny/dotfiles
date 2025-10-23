d5034008ed4bae00aaa396c5ccf252ea8f63d0b7
Jonas Lergell <jonaslergell@gmail.com>
2025-10-23 14:48:30 +0200

    configure file picker

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -5,16 +5,14 @@ return {
     enabled = false,
   },
 
-  -- Configure neo-tree to show hidden files
+  -- Configure snacks explorer to show hidden and ignored files by default
   {
-    "nvim-neo-tree/neo-tree.nvim",
+    "snacks.nvim",
     opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true, -- Show hidden files
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
+      picker = {
+        hidden = true, -- Show hidden files by default
+        ignored = true, -- Show gitignored files by default
+        exclude = { ".DS_Store" },
       },
     },
   },
