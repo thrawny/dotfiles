75751ed99853411020ea7bf2064b06b100545c47
Jonas Lergell <jonaslergell@gmail.com>
2025-11-03 21:46:43 +0100

    another context7 mcp install change

--- config/claude/install-mcp.sh
+++ config/claude/install-mcp.sh
@@ -5,8 +5,9 @@ set -eou pipefail
 shopt -s nullglob
 
 main() {
-    claude mcp add -s user --transport sse context7 https://mcp.context7.com/sse
-    claude mcp add -s user playwright npx @playwright/mcp@latest -- --caps vision
+  # claude mcp add -s user --transport sse context7 https://mcp.context7.com/sse
+  # claude mcp add context7 -- npx -y @upstash/context7-mcp
+  claude mcp add -s user playwright npx @playwright/mcp@latest -- --caps vision
 }
 
 main "$@"
