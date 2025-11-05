6b929787e62d925ba53272ce7adde08e4c901b8f
Jonas Lergell <jonaslergell@gmail.com>
2025-11-05 19:21:20 +0100

    hyprland window fixes

--- nix/home/nixos/hyprland/windows.nix
+++ nix/home/nixos/hyprland/windows.nix
@@ -4,23 +4,14 @@ _: {
       # Prevent sudden maximize events when spawning toolkits.
       "suppressevent maximize, class:.*"
 
-      # Keep Chromium-based apps tiled unless explicitly floated.
-      "tile, class:^(chromium)$"
-
       # Float common control panels.
       "float, class:^(org.pulseaudio.pavucontrol|blueberry.py|.blueman-manager-wrapped)$"
-      "float, class:^(steam)$"
       "float, class:^(1password)$"
       "float, class:^(nm-connection-editor)$"
-      "fullscreen, class:^(com.libretro.RetroArch)$"
 
       # Adjust opacity for a touch of depth without hampering focus.
       "opacity 0.97 0.9, class:.*"
-      "opacity 1 1, class:^(chromium|google-chrome|google-chrome-unstable)$, title:.*Youtube.*"
-      "opacity 1 0.97, class:^(chromium|google-chrome|google-chrome-unstable)$"
-      "opacity 0.97 0.9, initialClass:^(chrome-.*-Default)$"
-      "opacity 1 1, initialClass:^(chrome-youtube.*-Default)$"
-      "opacity 1 1, class:^(zoom|vlc|org.kde.kdenlive|com.obsproject.Studio|steam|cursor)$"
+      "opacity 1 1, class:^(cursor)$"
 
       # Smooth XWayland drag behaviour.
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
