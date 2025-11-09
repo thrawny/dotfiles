6e94b976550384be9e8f346fb19c9ecd2dfb05ab
Jonas Lergell <jonaslergell@gmail.com>
2025-11-09 19:46:49 +0100

    cleanup tmux-navigator

--- config/nvim/lua/plugins/tmux-navigator.lua
+++ config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,17 +1,3 @@
 return {
   "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", desc = "Navigate left (tmux-aware)" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", desc = "Navigate down (tmux-aware)" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", desc = "Navigate up (tmux-aware)" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", desc = "Navigate right (tmux-aware)" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", desc = "Navigate to previous (tmux-aware)" },
-  },
 }
