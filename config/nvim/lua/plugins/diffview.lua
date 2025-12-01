22cc19c79d43c62b62d042f2a850d12b4f123134
Jonas Lergell <jonaslergell@gmail.com>
2025-12-01 14:23:31 +0100

    trying diffview

---
+++ config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+    { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "File History (current)" },
+  },
+}
