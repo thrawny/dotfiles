75be9826bc32102e1fb9c138b174cf36b7669351
Jonas Lergell <jonaslergell@gmail.com>
2026-03-02 20:01:20 +0100

    chore(zmx): bump zmx to 0.4.1

--- nix/home/shared/zmx.nix
+++ nix/home/shared/zmx.nix
@@ -7,11 +7,11 @@ let
   inherit (pkgs.stdenv.hostPlatform) system;
   zmxBinary = pkgs.stdenvNoCC.mkDerivation {
     pname = "zmx";
-    version = "0.4.0";
+    version = "0.4.1";
     nativeBuildInputs = [ pkgs.installShellFiles ];
     src = pkgs.fetchurl {
-      url = "https://zmx.sh/a/zmx-0.4.0-linux-x86_64.tar.gz";
-      sha256 = "sha256-+ubSJrmPjf7qCXb/L57xYyzw6f2ky5IdCPKunr2Vo3g=";
+      url = "https://zmx.sh/a/zmx-0.4.1-linux-x86_64.tar.gz";
+      sha256 = "sha256-6bZbakDdXIj5toN2oFoOzGrHfuu7pE8WKfTbjoO2Eag=";
     };
     dontUnpack = true;
     installPhase = ''
