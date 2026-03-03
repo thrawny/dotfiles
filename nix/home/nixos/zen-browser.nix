a2b5b80e02f2449c7c03111f2e654bd675d4e1af
Jonas Lergell <jonas@lergell.se>
2026-03-03 17:34:10 +0100

    fix zen

--- nix/home/nixos/zen-browser.nix
+++ nix/home/nixos/zen-browser.nix
@@ -12,13 +12,13 @@
   # the package, the store path changes, creating a new Install ID and causing
   # Zen to start with a fresh profile.
   # MOZ_LEGACY_PROFILES=1 should fix this but doesn't work reliably with desktop launchers.
-  # The reliable fix is to pass -p "profile-name" directly.
+  # The reliable fix is to pass -P profile-name directly.
   # See: https://github.com/NixOS/nixpkgs/issues/58923
   # See: https://github.com/0xc000022070/zen-browser-flake/issues/179
   xdg.desktopEntries.zen-beta = {
     name = "Zen Browser (Beta)";
     genericName = "Web Browser";
-    exec = ''zen-beta --name zen-beta -p "Default Profile" %U'';
+    exec = ''zen-beta -P default %U'';
     icon = "zen-browser";
     terminal = false;
     type = "Application";
@@ -42,11 +42,11 @@
     actions = {
       new-window = {
         name = "New Window";
-        exec = ''zen-beta --new-window -p "Default Profile" %U'';
+        exec = ''zen-beta --new-window -P default %U'';
       };
       new-private-window = {
         name = "New Private Window";
-        exec = ''zen-beta --private-window -p "Default Profile" %U'';
+        exec = ''zen-beta --private-window -P default %U'';
       };
       profile-manager-window = {
         name = "Profile Manager";
