4f925e51ae2fea8ab88b7f4e0864571803673fc4
Jonas Lergell <jonas@lergell.se>
2026-04-01 10:25:27 +0200

    zmx main

--- nix/home/shared/zmx.nix
+++ nix/home/shared/zmx.nix
@@ -6,7 +6,7 @@
 }:
 let
   inherit (pkgs.stdenv.hostPlatform) system;
-  zmxPkg = zmx.packages.${system}.zmx;
+  zmxPkg = zmx.packages.${system}.zmx-main;
 in
 {
   home.packages = lib.optionals (builtins.hasAttr system zmx.packages) [
