a42caabd4343c0be281efc4b90baf8dde4aab04b
Jonas Lergell <jonas@lergell.se>
2026-04-06 15:54:33 +0200

    nvim update

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -53,7 +53,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.fzf" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.formatting.biome" },
+    { import = "lazyvim.plugins.extras.lang.typescript.biome" },
     { import = "lazyvim.plugins.extras.ai.supermaven" },
     { import = "lazyvim.plugins.extras.util.rest" },
     -- Conditionally import nix extra (only if nixd is available)
