f0cccabb7a4d2b841b70ac3de9eab8cab0e41448
Jonas Lergell <jonas@lergell.se>
2026-05-12 09:15:54 +0200

    add helium to 1password

--- nix/modules/nixos/1password.nix
+++ nix/modules/nixos/1password.nix
@@ -6,11 +6,15 @@
     polkitPolicyOwners = [ config.dotfiles.username ];
   };
 
-  # Allow Zen Browser to use 1Password browser extension
+  # Allow Nix-installed browsers to use the 1Password browser extension.
+  # Helium is installed as a system package in desktop.nix from the
+  # helium-browser flake; include both launcher and wrapped binary names.
   environment.etc."1password/custom_allowed_browsers" = {
     text = ''
       zen
       .zen-wrapped
+      helium
+      .helium-wrapped
       google-chrome
     '';
     mode = "0755";
