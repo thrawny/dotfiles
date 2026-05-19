eef54b2a7b065df6ba1e3fbabcae25df10dd96a1
Jonas Lergell <jonas@lergell.se>
2026-05-19 13:34:03 +0200

    Disable package install lifecycle scripts

--- nix/home/shared/npm.nix
+++ nix/home/shared/npm.nix
@@ -6,5 +6,12 @@
   # Configure npm to use a writable directory for global packages
   home.file.".npmrc".text = ''
     prefix = ${config.home.homeDirectory}/.npm-global
+    ignore-scripts = true
+  '';
+
+  # Prevent Bun installs from running lifecycle scripts by default.
+  home.file.".bunfig.toml".text = ''
+    [install]
+    ignoreScripts = true
   '';
 }
