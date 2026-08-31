25288f5ec130718bf0364c6481e72210a009d4d1
Jonas Lergell <jonas@lergell.se>
2026-08-31 16:37:19 +0200

    refactor(voxtype): move config into dedicated module

--- nix/home/nixos/default.nix
+++ nix/home/nixos/default.nix
@@ -3,7 +3,6 @@
 {
   pkgs,
   username,
-  voxtype,
   xremap-flake,
   ...
 }:
@@ -34,68 +33,8 @@
     ./waybar.nix
     ./gtk.nix
     ./viewers.nix
-
-    voxtype.homeManagerModules.default
+    ./voxtype.nix
   ];
-
-  programs.voxtype = {
-    enable = true;
-    package = voxtype.packages.${pkgs.stdenv.hostPlatform.system}.default;
-    service.enable = true;
-    settings = {
-      audio.device = "pipewire";
-      hotkey.enabled = false;
-      osd.frontend = "native";
-      output = {
-        mode = "type";
-        fallback_to_clipboard = true;
-        paste_keys = "shift+insert";
-      };
-      whisper = {
-        mode = "remote";
-        language = "en";
-        # Work around voxtype#496, which sends the local model name to remote APIs.
-        model = "whisper-large-v3-turbo";
-        remote_endpoint = "https://api.groq.com/openai";
-        remote_model = "whisper-large-v3-turbo";
-        remote_timeout_secs = 30;
-      };
-      text.replacements = {
-        "wavois" = "voxtype";
-        "nbin" = "nvim";
-        "n-bar" = "env var";
-        "n var" = "env var";
-        ".files" = "dotfiles";
-        "simlink" = "symlink";
-        "sim linking" = "symlinking";
-        "lasergit" = "lazygit";
-        "operational shock" = "operational soc";
-        "prequel" = "prequal";
-        "czu" = "csv";
-        "pubview" = "webview";
-        "guitar action" = "github action";
-        "throney" = "thrawny";
-        "cashics" = "cachix";
-        "kasex" = "Cachix";
-        "zms" = "zmx";
-        "groc" = "groq";
-        "deer" = "dir";
-        "in cus" = "incus";
-        "psuedo" = "sudo";
-        "cloudcod" = "claude code";
-        "Pyre" = "Pi";
-        "Kodis" = "Codex";
-        "zulius" = "solis";
-        "zulu" = "solis";
-        "veko" = "weco";
-        "cynic cell" = "sinexcel";
-        "cmx" = "zmx";
-        "inkis" = "incus";
-        "hermi" = "hermes";
-        ".file" = "dotfile";
-      };
-    };
-  };
 
   home = {
     inherit username;
@@ -104,7 +43,6 @@
 
     packages = with pkgs; [
       vesktop # Discord client with Wayland screen sharing support
-      voxtype.packages.${pkgs.stdenv.hostPlatform.system}.osd-native
     ];
   };
 }
---
+++ nix/home/nixos/voxtype.nix
@@ -0,0 +1,71 @@
+{
+  pkgs,
+  voxtype,
+  ...
+}:
+{
+  imports = [ voxtype.homeManagerModules.default ];
+
+  programs.voxtype = {
+    enable = true;
+    package = voxtype.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    service.enable = true;
+    settings = {
+      audio.device = "pipewire";
+      hotkey.enabled = false;
+      osd.frontend = "native";
+      output = {
+        mode = "type";
+        fallback_to_clipboard = true;
+        paste_keys = "shift+insert";
+      };
+      whisper = {
+        mode = "remote";
+        language = "en";
+        # Work around voxtype#496, which sends the local model name to remote APIs.
+        model = "whisper-large-v3-turbo";
+        remote_endpoint = "https://api.groq.com/openai";
+        remote_model = "whisper-large-v3-turbo";
+        remote_timeout_secs = 30;
+      };
+      text.replacements = {
+        "neary" = "Niri";
+        "veiland" = "Wayland";
+        "neovim" = "Neovim";
+        "nbin" = "nvim";
+        "n-bar" = "env var";
+        "n var" = "env var";
+        ".files" = "dotfiles";
+        "simlink" = "symlink";
+        "sim linking" = "symlinking";
+        "lasergit" = "lazygit";
+        "operational shock" = "operational soc";
+        "prequel" = "prequal";
+        "czu" = "csv";
+        "pubview" = "webview";
+        "guitar action" = "github action";
+        "throney" = "thrawny";
+        "cashics" = "cachix";
+        "kasex" = "Cachix";
+        "zms" = "zmx";
+        "groc" = "groq";
+        "deer" = "dir";
+        "in cus" = "incus";
+        "psuedo" = "sudo";
+        "cloudcod" = "claude code";
+        "Pyre" = "Pi";
+        "Kodis" = "Codex";
+        "zulius" = "solis";
+        "zulu" = "solis";
+        "veko" = "weco";
+        "cynic cell" = "sinexcel";
+        "cmx" = "zmx";
+        "inkis" = "incus";
+        "hermi" = "hermes";
+        ".file" = "dotfile";
+      };
+    };
+  };
+
+  home.packages = [ voxtype.packages.${pkgs.stdenv.hostPlatform.system}.osd-native ];
+}
