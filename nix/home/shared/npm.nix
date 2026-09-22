276a28a3629a033b6e044a0509ed4ac93d5dbeb7
Jonas Lergell <jonas@lergell.se>
2026-09-22 14:01:20 +0200

    add npm token

--- nix/home/shared/npm.nix
+++ nix/home/shared/npm.nix
@@ -7,6 +7,10 @@
   home.file.".npmrc".text = ''
     prefix = ${config.home.homeDirectory}/.npm-global
     ignore-scripts = true
+
+    # Token comes from NPM_TOKEN, exported by ~/.secrets, so it stays out of
+    # the world-readable nix store.
+    //npm.pkg.github.com/:_authToken=''${NPM_TOKEN}
   '';
 
   # Prevent Bun installs from running lifecycle scripts by default.
